1c8b3f967b254d804dada1ef65d8272ee826fefe
dgusfr <diegogustavo.fr@gmail.com>
2024-06-15 12:10:25 -0300

    add public

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const express = require("express");
 const app = express(); //instance of express
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/:nome/:lang", (req, res) => {
   var name = req.params.nome;
   var lang = req.params.lang;
-  var exibirMSG = true;
-  //renderizando html na tela:
+  var exibirMSG = false;
+
+  var products = [
+    { name: "Playstation 5", price: 4499 },
+    { name: "Xbox Series X", price: 4999 },
+    { name: "Nintendo Switch", price: 2999 },
+  ];
+
   res.render("index", {
     name: name,
     lang: lang,
     msg: exibirMSG,
+    products: products,
   });
 });
 
